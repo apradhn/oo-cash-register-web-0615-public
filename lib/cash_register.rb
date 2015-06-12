e87809146efd8f8a141ef82590f9cd1ee6453fa6
apradhn <abhijit.pradhan6@gmail.com>
2015-06-12 14:32:44 -0400

    all done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require "pry"
+
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount, :items
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+    apply_discount if discount > 0
+  end
+
+  def add_item(item, price, quantity=1)
+    quantity.times {|i| @items << item}
+    @total += price*quantity
+  end
+
+  def apply_discount
+    if total == 0.0
+      "There is no discount to apply."
+    else
+      @total -= @total*(discount_percent)
+      "After the discount, the total comes to $#{@total.round}."
+    end
+  end
+
+  def discount_percent
+    @discount * 0.01
+  end
+
+end
